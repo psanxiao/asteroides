5be9f38569fe080bf068e2841e151e25d409c3a5
psanxiao <psanxiao@Duibhe.(none)>
2013-02-28 20:19:47 +0100

    Initial import

---
+++ src/com/example/asteroides/Asteroides.java
@@ -0,0 +1,33 @@
+package com.example.asteroides;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class Asteroides extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+	public void lanzarAcercaDe(View view) {
+		Intent i = new Intent(this, AcercaDe.class);
+		startActivity(i);
+	}
+	
+	public void exit(View view) {
+		finish();
+	}
+    
+}
