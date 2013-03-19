77dc42cee2dd79184528b63b3c372b9a8d4e2b5d
psanxiao <psanxiao@Duibhe.(none)>
2013-03-19 22:33:29 +0100

    Added menu and preferences

--- src/com/example/asteroides/Asteroides.java
+++ src/com/example/asteroides/Asteroides.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Asteroides extends Activity {
+	
+	public static AlmacenPuntuacionesArray almacen = new AlmacenPuntuacionesArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +20,37 @@ public class Asteroides extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        inflater.inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.acercaDe:
+    		lanzarAcercaDe(null);
+    		break;
+    	case R.id.config:
+    		lanzarPreferencias(null);
+    		break;
+    	}
+    	return true;
     }
     
 	public void lanzarAcercaDe(View view) {
 		Intent i = new Intent(this, AcercaDe.class);
+		startActivity(i);
+	}
+	
+	public void lanzarPreferencias(View view) {
+		Intent i = new Intent(this, Preferencias.class);
+		startActivity(i);
+	}
+	
+	public void lanzarPuntuaciones(View view) {
+		Intent i = new Intent(this, Puntuaciones.class);
 		startActivity(i);
 	}
 	
