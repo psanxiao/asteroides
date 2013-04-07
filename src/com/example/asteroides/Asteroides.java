e22704b0e56f8c54022085f2a1000d1da4fd51c7
psanxiao <psanxiao@Duibhe.(none)>
2013-04-07 19:54:42 +0200

    Created Juego Activity

--- src/com/example/asteroides/Asteroides.java
+++ src/com/example/asteroides/Asteroides.java
@@ -54,6 +54,11 @@ public class Asteroides extends Activity {
 		startActivity(i);
 	}
 	
+	public void lanzarJuego(View view) {
+		Intent i = new Intent(this, Juego.class);
+		startActivity(i);
+	}
+	
 	public void exit(View view) {
 		finish();
 	}
