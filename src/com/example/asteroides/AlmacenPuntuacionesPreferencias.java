fd0208dc556d4685edaa1b4a2bf6a8d4820a97e8
psanxiao <psanxiao@Duibhe.(none)>
2013-04-21 13:03:03 +0200

    Added AlmacenPuntuacionesPreferencias class

---
+++ src/com/example/asteroides/AlmacenPuntuacionesPreferencias.java
@@ -0,0 +1,35 @@
+package com.example.asteroides;
+
+import java.util.Vector;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AlmacenPuntuacionesPreferencias implements AlmacenarPuntuaciones {
+    private static String PREFERENCIAS = "puntuaciones";
+    private Context context;
+
+    public AlmacenPuntuacionesPreferencias(Context context) {
+          this.context = context;
+    }
+
+    public void guardarPuntuacion(int puntos, String nombre, 
+                                                                                                        long fecha) {
+          SharedPreferences preferencias =context.getSharedPreferences(
+                        PREFERENCIAS, Context.MODE_PRIVATE);
+          SharedPreferences.Editor editor = preferencias.edit();
+          editor.putString("puntuacion", puntos + " " + nombre);
+          editor.commit();
+    }
+
+    public Vector<String> listaPuntuaciones(int cantidad) {
+          Vector<String> result = new Vector<String>();
+          SharedPreferences preferencias =context.getSharedPreferences(
+                                                                PREFERENCIAS, Context.MODE_PRIVATE);
+          String s = preferencias.getString("puntuacion", "");
+          if (s != "") {
+                 result.add(s);
+          }
+          return result;
+    }
+}
